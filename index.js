ef5e04f1bb5b6c201c2c5d287f4b3b26ab4a1278
Charlotte Payne <cltpayne@gmail.com>
2018-02-27 23:49:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -127,11 +127,12 @@ function endGame() {
 function moveDodger(e) {
  if (e.which === LEFT_ARROW) {
   moveDodgerLeft();
-    e.stopPropagation();
-    e.preventDefault();
-  } else if (e.which === RIGHT_ARROW) {
-    moveDodgerRight();
-        e.preventDefault();
+  e.stopPropagation();
+  e.preventDefault();
+  } 
+  else if (e.which === RIGHT_ARROW) {
+  moveDodgerRight();
+  e.preventDefault();
   }
   // implement me!
   /**
