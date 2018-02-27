39140280500da5518e7cd588be202d481a6ed335
Charlotte Payne <cltpayne@gmail.com>
2018-02-27 23:36:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ function endGame() {
   clearInterval(gameInterval)
   ROCKS.forEach(function(rock) {rock.remove();})
   window.removeEventListener('keydown', moveDodger)
-  return alert("YOU LOSE!")
+  alert("YOU LOSE!")
 }
 
 function moveDodger(e) {
